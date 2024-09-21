8762a7b8203fc090aee08277d2153466eb1bca57
jitu7033 <jraj4797@gmail.com>
2024-09-21 10:42:28 +0530

    ccommit initial

--- basic_project/src/App.jsx
+++ basic_project/src/App.jsx
@@ -27,7 +27,7 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-        hlo jitendra
+        hlo jitendra jasdhfjsdhf
       </p>
     </>
   )
